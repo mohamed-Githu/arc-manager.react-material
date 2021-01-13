6ce0c0bd3f2417a8ba5e0c42f77bc4ba73a915f3
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2021-01-14 01:39:40 +0200

    Added the initial input fields including to date picker to the modal

--- src/components/ui/add-modal/add-modal.component.jsx
+++ src/components/ui/add-modal/add-modal.component.jsx
@@ -1,17 +1,71 @@
+import { useState } from "react";
+
 import useStyles from "./add-modal.styles";
-import { Modal, Grid, Typography } from "@material-ui/core";
+import {
+  Modal,
+  Grid,
+  Typography,
+  TextField,
+  InputAdornment,
+} from "@material-ui/core";
+import { KeyboardDatePicker } from "@material-ui/pickers";
 
 const AddModal = (modalProps) => {
   const classes = useStyles();
 
+  const [date, handleDateChange] = useState(new Date());
+  const [input, setInput] = useState({
+    name: "",
+    total: "",
+  });
+
+  const { name, total } = input;
+  const handleChange = (e) =>
+    setInput({ ...input, [e.target.id]: e.target.value });
+
   return (
     <Modal {...modalProps}>
       <Grid container className={classes.modal}>
-        <Grid container className={classes.modal}>
-          <Grid item>
-            <Typography variant="h2" align="center" gutterBottom>
-              Add a new project
-            </Typography>
+        <Grid item>
+          <Typography variant="h2" align="center" gutterBottom>
+            Add a new project
+          </Typography>
+        </Grid>
+        <Grid item container justify="space-between" alignItems="flex-end">
+          <Grid item container direction="column" sm>
+            <Grid item>
+              <TextField
+                id="name"
+                label="Name"
+                value={name}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Grid item container direction="column" alignItems="center" sm>
+            <Grid item>
+              <KeyboardDatePicker
+                format="MM/dd/yyyy"
+                value={date}
+                onChange={handleDateChange}
+              />
+            </Grid>
+          </Grid>
+          <Grid item container direction="column" alignItems="flex-end" sm>
+            <Grid item>
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">$</InputAdornment>
+                  ),
+                }}
+                type="number"
+                label="Total"
+                id="total"
+                value={total}
+                onChange={handleChange}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
