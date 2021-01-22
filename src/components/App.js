da7e606d294c8612dc208554808669292e7dc483
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2021-01-22 19:56:41 +0200

    Added the undo functionality with the snackbar material-ui component to the app, so that the use can undo the delete action before the alert message goes away

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import {
   Grid,
   TextField,
   FormGroup,
+  Snackbar,
+  Button,
 } from '@material-ui/core';
 import AddIcon from "@material-ui/icons/Add";
 
@@ -20,6 +22,8 @@ import AddModal from "./ui/add-modal/add-modal.component";
 import theme from './ui/Theme';
 
 const App = () => {
+  const classes = useStyles();
+
   const [inputValue, setInputValue] = useState("");
 
   const [websiteSwitch, setWebsiteSwitch] = useState(false);
@@ -30,16 +34,42 @@ const App = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const handleModal = () => setModalOpen(!modalOpen);
 
-  const [rows, setRows] = useState([]);
+  const [projects, setProjects] = useState({
+    rows: [],
+    deletedRows: [],
+  });
+  const { rows, deletedRows } = projects;
 
-  const classes = useStyles();
+  const [alert, setAlert] = useState({
+    open: false,
+    message: "Projects Deleted!",
+    color: "#FF3232",
+  });
 
   const handleInput = (e) => setInputValue(e.target.value);
-  const addRow = (newRow) => setRows([...rows, newRow]);
+  const addRow = (newRow) => setProjects({...projects, rows: [...rows, newRow]});
 
-  const deleteRows = (selectedRows) => setRows(
-    rows.filter(row => !selectedRows.includes(row.name))
-  );
+  const deleteRows = (selectedRows) => {
+    const _deletedRows = [];
+    const updatedRows = rows.filter((row, i) => {
+      if (!selectedRows.includes(row.name))
+        return true;
+      _deletedRows.push(rows[i]);
+      return false;
+    });
+    setProjects({deletedRows: _deletedRows, rows: updatedRows});
+    setAlert({...alert, open: true});
+  }
+
+  const onCloseAlert = () => {
+    setProjects({...projects, deletedRows: []});
+    setAlert({...alert, open: false})
+  };
+
+  const onUndo = () => {
+    setProjects({deletedRows: [], rows: [...rows, ...deletedRows]});
+    setAlert({...alert, open: false});
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -93,6 +123,21 @@ const App = () => {
       </Grid>
       <Footer />
       <AddModal open={modalOpen} addRow={addRow} rows={rows} onClose={handleModal} />
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        autoHideDuration={4000}
+        open={alert.open}
+        onClose={onCloseAlert}
+        message={alert.message}
+        ContentProps={{
+          style: {
+            backgroundColor: alert.color,
+          },
+        }}
+        action={<Button onClick={onUndo} style={{color: "#FFF"}}>
+          Undo
+        </Button>}
+      />
     </ThemeProvider>
   );
 }
