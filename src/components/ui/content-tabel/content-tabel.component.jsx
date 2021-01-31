aff96d13a3ee3f22644b2a98651ae8bbbf8073ec
Mohamed Hussein <mohamed.hussein151515@gmail.com>
2021-01-31 22:42:23 +0200

    Moved the utilities of the content-table to a seperate file

--- src/components/ui/content-tabel/content-tabel.component.jsx
+++ src/components/ui/content-tabel/content-tabel.component.jsx
@@ -29,40 +29,12 @@ import {
   Tooltip,
 } from "@material-ui/core";
 
-const getFilterdRows = (searchValue, rows) =>
-  rows.filter((row) => {
-    for (const rowItem of Object.values(row)) {
-      if (rowItem.toLowerCase().includes(searchValue.toLowerCase()))
-        return true;
-    }
-    return false;
-  });
-
-const descendingComparator = (a, b, orderBy) => {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-};
-
-const getComparator = (order, orderBy) => {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-};
-
-const stableSort = (array, comparator) => {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-};
+import {
+  getComparator,
+  getFilterdRows,
+  descendingComparator,
+  stableSort,
+} from "./content-table.utils";
 
 const headCells = [
   { id: "name", label: "Name" },
